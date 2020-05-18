74f1ba0ad5018561e0c96438d635c9ab89e3e6de
devlinjunker <devlin.junker@gmail.com>
2020-05-17 21:20:51 -0700

    test... will this fuckitup?

--- scripts/actions/collect-wiki.js
+++ scripts/actions/collect-wiki.js
@@ -14,18 +14,18 @@ if (!fs.existsSync(tmpPath)) {
 const map = {};
 const readmeFiles = esdocSettings['plugins'][0]['option']['manual']['files'];
 for (const i in readmeFiles) {
-  const fullPath = `../.${  readmeFiles[i]}`;
+  const fullPath = readmeFiles[i];
   const name = path.parse(fullPath).base;
 
-  const newName = `${i  }-${  name.substr(7, name.length - 3)}`;
+  const newName = `${i}-${name.substr(7, name.length - 3)}`;
 
   map[name] = newName;
-  fs.copyFileSync(path.resolve(__dirname, fullPath), path.join(tmpPath, newName));
+  fs.copyFileSync(path.resolve(__dirname, '../..', fullPath), path.join(tmpPath, newName));
 }
 
 // Attempt copy index file to "home.md" in wiki
 const home = esdocSettings['plugins'][0]['option']['manual']['index'];
-const homePath = `../.${home}`;
+const homePath = `../../${home}`;
 let content = fs.readFileSync(path.resolve(__dirname, homePath)).toString();
 
 // Replace all links in index.md file so they work on github
