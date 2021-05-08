9c78a482fdba93f0b09ec12bdbdaeb8ac9c51b06
Devlin Junker <devlin.junker@gmail.com>
2021-05-08 08:30:05 -0700

    add notes on require.context

--- test/test.bootstrap.js
+++ test/test.bootstrap.js
@@ -7,6 +7,8 @@ chai.use(sinonChai);
 global.expect = chai.expect;
 global.sandbox = sinon.createSandbox();
 
+// require.context() is a webpack feature
+// see https://webpack.js.org/guides/dependency-management/#requirecontext
 // $FlowFixMe
 const testsContext = require.context('../src', true, /.spec$/);
 
